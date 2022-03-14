81f5cd57b4436162368327bb4b08f67534284192
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-03-14 13:13:52 +0200

    deploy heroku

--- server/DAL/db.js
+++ server/DAL/db.js
@@ -1,13 +1,7 @@
+require('dotenv').config({
+    path: './.env'
+})
 const mongoose = require('mongoose')
 
-const username = "saramazal";
-const password = "classwork";
-connectionString = `mongodb+srv://${username}:${password}@mongo-lesson.ragui.mongodb.net/mongo-lesson?retryWrites=true&w=majority`
-
-
-
-console.log('connecting ..')
-mongoose.connect(connectionString)
-    .then(() => {
-        console.log('Mongo connected!')
-    })
+mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => console.log('Connect Wel Done!'))
--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
+require('dotenv').config({ path: './.env'})
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+
 app.use(require('cors')())
 app.use(express.json())
+//middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 require('./router')(app)
 
-const PORT = 3030
+const PORT = process.env.PORT || 3030
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`Server is running: ${PORT}`))
