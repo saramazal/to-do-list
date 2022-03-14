98af415eab7eb9dc70dc9da4cf82e55c5e95a20f
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-03-14 14:00:19 +0200

    build

--- client/src/index.js
+++ client/src/index.js
@@ -3,23 +3,9 @@ import ReactDOM from 'react-dom';
 import './style.css'
 import App from './App';
 
-ReactDOM.render( <
-  React.StrictMode >
-<<<<<<< HEAD
+ReactDOM.render( 
+  <React.StrictMode >
   <App / >
   </React.StrictMode>,
   document.getElementById('root')
 );
-=======
-  <
-  App / >
-  <
-  /React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 2565a01befb00b31f535689f754a85b546076681
